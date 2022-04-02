437c21f1ed328d4336ffed6da7987cdd573b7a9a
Vitor Melo <vitor.vmelo@tdx.com.br>
2022-04-02 19:48:41 -0300

    Coolant

--- computador_bordo.ino
+++ computador_bordo.ino
@@ -9,6 +9,8 @@ BluetoothSerial SerialBT;
 #define DEBUG_PORT Serial
 #define oled Heltec.display
 
+int button_state;
+
 ELM327 myELM327;
 
 uint32_t coolant = 0;
@@ -19,12 +21,32 @@ void setup()
 {
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
   pinMode(0, INPUT);
-  SerialBT.setPin("1234");
+  // SerialBT.setPin("1234");
   ELM_PORT.begin("ArduHUD", true);
 
-  connectOBD();
+  util.connectionOled("CONNECTING...", obd2_width, obd2_height, obd2_bits);
 
-  connectECU();
+  if (!ELM_PORT.connect("OBDII"))
+  {
+    util.connectionOled("NOT CONNECTED", obd2_width, obd2_height, obd2_bits);
+    while (1)
+      ;
+  }
+
+  util.connectionOled("CONNECTED", obd2_width, obd2_height, obd2_bits);
+  delay(3000);
+
+  util.connectionOled("CONNECTING...", engine_width, engine_height, engine_bits);
+
+  if (!myELM327.begin(ELM_PORT, true, 2000))
+  {
+    util.connectionOled("NOT CONNECTED", engine_width, engine_height, engine_bits);
+    while (1)
+      ;
+  }
+
+  util.connectionOled("NOT CONNECTED", engine_width, engine_height, engine_bits);
+  delay(3000);
 }
 
 void loop()
@@ -39,35 +61,4 @@ void loop()
     util.connectionOled("ELM Disconnected", disconnected_width, disconnected_height, disconnected_bits);
     myELM327.printError();
   }
-}
-
-void connectOBD()
-{
-
-  util.connectionOled("CONNECTING...", obd2_width, obd2_height, obd2_bits);
-
-  if (!ELM_PORT.connect("OBDII"))
-  {
-    util.connectionOled("NOT CONNECTED", obd2_width, obd2_height, obd2_bits);
-    while (1)
-      ;
-  }
-
-  util.connectionOled("CONNECTED", obd2_width, obd2_height, obd2_bits);
-  delay(3000);
-}
-
-void connectECU()
-{
-  util.connectionOled("CONNECTING...", engine_width, engine_height, engine_bits);
-
-  if (!myELM327.begin(ELM_PORT, true, 2000))
-  {
-    util.connectionOled("NOT CONNECTED", engine_width, engine_height, engine_bits);
-    while (1)
-      ;
-  }
-
-  util.connectionOled("NOT CONNECTED", engine_width, engine_height, engine_bits);
-  delay(3000);
 }
